76bb32af7d5ecd5b41475b96bfd3f689c188343d
Muhammed Thanish <mnmtanish@gmail.com>
2025-01-23 11:07:51 +0100

    fix: fix app crash with some city names

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.thanish.prayers.se"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.7"
+        versionCode = 9
+        versionName = "1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
