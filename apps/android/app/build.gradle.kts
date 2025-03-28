d0773de9c0a2531c9130c6cf0ca4708d99042dab
Thanish Nizam <mnmtanish@gmail.com>
2025-03-28 20:47:18 +0100

    chore: bump android project dependencies

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "me.thanish.prayers.se"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "4.1.0"
+        versionCode = 33
+        versionName = "4.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
